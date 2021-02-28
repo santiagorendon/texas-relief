c908f43f09f8bac09dc4dbfbe60891d4e6d1a378
Santiago Rendon <sr4670@nyu.edu>
2021-02-28 11:48:30 -0500

    save

--- src/server/index.js
+++ src/server/index.js
@@ -9,4 +9,8 @@ app.get('/api/getUsername', (req, res) => {
   res.send({ username: os.userInfo().username });
 });
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('/src/client/build'))
+}
+
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
