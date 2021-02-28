45ed9f6ea43e39fecc12ee5d8ff44b022b929f79
Nilay Neeranjun <nneeranjun@guidewire.com>
2021-02-28 02:57:31 -0500

    added tags, not updating

--- src/client/components/News.js
+++ src/client/components/News.js
@@ -10,9 +10,9 @@ class News extends React.Component{
   constructor() {
     super();
     this.state = {
-        articles: []
+        articles: [],
+        tags: [["Relief", true], ["Disaster", false], ["Outage", false], ["Blizzard", false], ["Donation", false]]
     }
-    this.articles = []
     this.componentDidMount = this.componentDidMount.bind(this);
   }
 
@@ -26,7 +26,7 @@ class News extends React.Component{
 
   componentDidMount() {
     this.navHighlight();
-      this.getNews()
+      this.getNews(this.state.tags)
         .then(res => {
             this.setState({ articles: JSON.parse(res).value })
             console.log(JSON.parse(res).value[0])
@@ -36,16 +36,16 @@ class News extends React.Component{
         })
   }
 
-   getNews() {
+   getNews(tags) {
     let subscriptionKey = '8d6cbfdda76342e0afe828481020bd84';
     let host = 'api.bing.microsoft.com';
     let path = '/v7.0/news/search';
-    let searchTerm = "texas AND (disaster OR outage)";
-    let count = 30;
+    console.log("texas && " + this.state.tags.filter(tag => tag[1] == true).map(tag => tag[0].toLocaleLowerCase()).join(" || "));
+    let searchTerm = "texas+" + this.state.tags.filter(tag => tag[1] == true).map(tag => tag[0].toLocaleLowerCase()).join("+")
     return new Promise((resolve, reject) => {
       https.get({
         hostname: host,
-        path:     path + `?q=${encodeURIComponent(searchTerm)}&count=${count}`,
+        path:     path + `?q=${encodeURIComponent(searchTerm)}`,
         headers:  { 'Ocp-Apim-Subscription-Key': subscriptionKey },
       }, res => {
         let body = ''
@@ -61,12 +61,42 @@ class News extends React.Component{
     })
 }
 
+selectTag(index) {
+  var newTags = this.state.tags.slice()
+  newTags[index][1] = !newTags[index][1]
+  this.setState({
+    tags: newTags
+  })
+  this.getNews(this.state.tags)
+        .then(res => {
+            this.setState({ articles: JSON.parse(res).value })
+            console.log(JSON.parse(res).value[0])
+        })
+        .catch(e => {
+            console.log(e)
+    })
+
+}
+
   render() {
     if (this.state.articles.length > 0) {
         return (
           <div>
             <NavBar />
             <div className="container" style = {{display: 'block'}}>
+              <div className="row container">
+                {this.state.tags.map((value, index) => {
+                  if (!value[1]) {
+                    return (
+                      <button type="button" class="btn btn-outline-secondary news-tag" onClick={() => this.selectTag(index)}>{value[0]}</button>
+                  )
+                  } else {
+                    return (
+                      <button type="button" class="btn btn-outline-secondary news-tag" onClick={() => this.selectTag(index)} style= {{backgroundColor:"#6C757D", color: 'white'}}>{value[0]}</button>
+                    )
+                  }
+                })}
+              </div>
                 <div className="row"> 
                     {this.state.articles.map((value, index) => {
                         return (
--- src/client/components/NewsArticle.js
+++ src/client/components/NewsArticle.js
@@ -13,7 +13,7 @@ class NewsArticle extends React.Component{
     return (
       <a href={this.props.article.url} className="card-link" target="_blank">
           <div className="card text-center shadow-sm p-3 mb-5">
-              <img className="card-img-top" src={this.props.article.image.thumbnail.contentUrl} alt="Card image cap"></img>
+              <img className="card-img-top" src={this.props.article?.image.thumbnail.contentUrl} alt="Card image cap"></img>
               <div className="card-body">
                   <h5 className="card-title max-lines">{this.props.article.name}</h5>
               </div>
