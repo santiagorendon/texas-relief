115bbe5056803e94ff539bd0e5c53bf31c7b3207
Santiago Rendon <sr4670@nyu.edu>
2021-03-04 16:35:51 -0500

    test

--- src/server/index.js
+++ src/server/index.js
@@ -92,7 +92,8 @@ app.post('/api/create-post', (req, res) => {
 })
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('/src/dist'));
+  //app.use(express.static('/src/dist'));
+  app.use(express.static(path.join(__dirname, 'dist')));
   //app.get('*', (req, res)=>res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')));
 }
 
